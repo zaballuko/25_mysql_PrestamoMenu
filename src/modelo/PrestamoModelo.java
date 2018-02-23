a849e3a167a4e0e8e02054537af8bdfb94535f9e
davidZabal <zaballuko@gmail.com>
2018-02-23 09:02:09 +0100

    first commit

---
+++ src/modelo/PrestamoModelo.java
@@ -0,0 +1,58 @@
+package modelo;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+
+
+public class PrestamoModelo extends Conector{
+
+	LibroModelo libroModelo = new LibroModelo();
+	UsuarioModelo usuarioModelo = new UsuarioModelo();
+	ArrayList<Prestamo> prestamos = new ArrayList<Prestamo>();
+	
+	public void insertar(Prestamo prestamo){
+		try {
+			PreparedStatement pst = super.conexion.prepareStatement("insert into prestamos (id_libro, id_usuario, fecha_prestamo, fecha_limite, entregado) values(?,?,?,?,?)");
+			pst.setInt(1, prestamo.getLibro().getId());
+			pst.setInt(2, prestamo.getUsuario().getId());
+			pst.setDate(3, new java.sql.Date(prestamo.getFechaPrestamo().getTime()));
+			pst.setDate(4, new java.sql.Date(prestamo.getFechaLimite().getTime()));
+			pst.setBoolean(5, prestamo.isEntregado());
+			pst.execute();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public ArrayList<Prestamo> selectAll(){
+		Statement st;
+		Prestamo prestamo;
+		ArrayList<Prestamo> prestamos = new ArrayList<Prestamo>();
+		try {
+			st = super.conexion.createStatement();
+			ResultSet rs = st.executeQuery("select * from prestamos");
+			while(rs.next()){
+				prestamo = new Prestamo();
+				prestamo.setId(rs.getInt("id"));
+				prestamo.setLibro(libroModelo.select(rs.getInt("id")));
+				prestamo.setUsuario(usuarioModelo.select(rs.getInt("id")));
+				prestamo.setFechaPrestamo(rs.getDate("fecha_prestamo"));
+				prestamo.setFechaLimite(rs.getDate("fecha_limite"));
+				prestamo.setEntregado(rs.getBoolean("entregado"));
+				
+				prestamos.add(prestamo);
+			
+			}
+			return prestamos;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return prestamos;
+	}
+}
---
+++ src/vista/PrestamoVista.java
@@ -0,0 +1,137 @@
+package vista;
+
+import java.util.Date;
+import java.util.Scanner;
+
+import javax.management.modelmbean.ModelMBeanOperationInfo;
+
+import modelo.Libro;
+import modelo.LibroModelo;
+import modelo.Prestamo;
+import modelo.PrestamoModelo;
+import modelo.Usuario;
+import modelo.UsuarioModelo;
+
+public class PrestamoVista {
+
+	static final int TOMAR_PRESTADO = 1;
+	static final int ENTREGAR = 2;
+	static final int SALIR = 0;
+	
+	public void menuPrestamo(){
+		int opcion;
+		Scanner scan = new Scanner(System.in);
+		do{
+			System.out.println("--Prestamos--");
+			System.out.println(TOMAR_PRESTADO + " tomar prestado un libro");
+			System.out.println(ENTREGAR + " entregar un libro");
+			opcion = Integer.parseInt(scan.nextLine());
+			
+			switch (opcion) {
+			case TOMAR_PRESTADO:
+				realizarPrestamo(scan);
+				break;
+			case ENTREGAR:
+				
+				break;
+			default:
+				break;
+			}
+			
+		}while(opcion != SALIR);	
+	}
+
+	
+	
+	private void realizarPrestamo(Scanner scan) {
+		System.out.println("Introduce el titulo del libro");
+		String titulo = scan.nextLine();
+		LibroModelo libroModelo = new LibroModelo();
+		Libro libro = libroModelo.selectPorTitulo(titulo);
+		
+		if(libro != null){//el libro existe
+			System.out.println("Introduce el DNI");
+			String dni = scan.nextLine();
+			UsuarioModelo usuarioModelo = new UsuarioModelo();
+			Usuario usuario = usuarioModelo.selectPorDni(dni);
+			
+			//crear el objeto prestamo 
+			Prestamo prestamo = new Prestamo();
+
+			prestamo.getLibro().getId();
+			prestamo.getUsuario().getId();
+			
+			Date fechaPrestamo = new Date();
+			//limite es el dia de hoy mas 21 dias
+			Date fechaLimite = new Date(fechaPrestamo.getTime()+(21*24*60*60*1000));
+			prestamo.setFechaPrestamo(fechaPrestamo);
+			prestamo.setFechaLimite(fechaLimite);
+			
+			prestamo.setEntregado(false);
+			
+			
+			//crear el objeto modeloprestamo
+			PrestamoModelo prestamoModelo = new PrestamoModelo();
+			//insertar prestamo utilizando modeloPrestamo
+			prestamoModelo.insertar(prestamo);
+			System.out.println("Prestamo realizado");
+			
+		}else{//el libro no existe
+			System.out.println("Prestamo no realizado, el libro no existe");
+		}
+		
+	}
+	public void menulistar(){
+		int opcion;
+		Scanner scan = new Scanner(System.in);
+		do{
+			System.out.println("--Prestamos--");
+			System.out.println(TOMAR_PRESTADO + " tomar prestado un libro");
+			System.out.println(ENTREGAR + " entregar un libro");
+			opcion = Integer.parseInt(scan.nextLine());
+			
+			switch (opcion) {
+			case TOMAR_PRESTADO:
+				realizarPrestamo(scan);
+				break;
+			case ENTREGAR:
+				
+				break;
+			default:
+				break;
+			}
+			
+		}while(opcion != SALIR);	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
